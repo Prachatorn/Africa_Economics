551c90549526651d74230a188eae46bf6479c2a9
somchai joemjumroon <Prachatornjoe@gmail.com>
2019-11-17 16:10:29 -0800

    Worked on Graphs

--- africa_economic.R
+++ africa_economic.R
@@ -15,21 +15,28 @@ algeria <- africa %>%
 
 algeria_exchange_rate <- ggplot(algeria, aes(x = year, y = exch_usd)) +
   geom_line() +   
-  scale_y_discrete(limits = seq(from = 0, to = 100, by = 10))
+  scale_y_discrete(limits = seq(from = min(algeria$exch_usd), to = max(algeria$exch_usd), by = 10)) +
+  ggtitle(paste(algeria$country, "Exchange Rates From", min(algeria$year), " To ", max(algeria$year))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_line(aes(x = year, y = 0))
 
+
+png("Exchange Rates/Algeria.png")
 algeria_exchange_rate
+dev.off()
 
 # Inflation Rate
 
 algeria_inflation <- ggplot(algeria, aes(x = year, y = inflation_annual_cpi)) +
   geom_line() +
-  scale_x_discrete(limits = seq(from = 1870, to = 2014, by = 10)) +
-  ggtitle("Algeria Annual Inflation From 1870 To 2014") 
-
-algeria_inflation <- algeria_inflation +
+  scale_x_discrete(limits = seq(from = min(algeria$year), to = max(algeria$year), by = 10)) +
+  ggtitle(paste(algeria$country, "Annual Inflation From", min(algeria$year), "To", max(algeria$year))) + 
+  theme(plot.title = element_text(hjust = 0.5)) +
   geom_line(aes(x = year, y = 0))
 
+png("Inflation Rates/Algeria.png")
 algeria_inflation
+dev.off()
 
 # Angola
 
@@ -42,22 +49,28 @@ angola <- africa %>%
 # Exchange Rate
 
 angola_exchange_rate <- ggplot(angola, aes(x = year, y = exch_usd)) +
-  geom_line() +   
-  scale_y_discrete(limits = seq(from = 0, to = 100, by = 10))
+  geom_line() +
+  scale_y_discrete(limits = seq(from = min(angola$exch_usd), to = max(angola$exch_usd), by = 10)) +
+  ggtitle(paste(angola$country, "Exchange Rates From", min(angola$year), "To", max(angola$year))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_line(aes(x = year, y = 0))
 
+png("Exchange Rates/Angola.png")
 angola_exchange_rate
+dev.off()
 
 # Inflation Rate
 
 angola_inflation <- ggplot(angola, aes(x = year, y = inflation_annual_cpi)) +
   geom_line() +
-  scale_x_discrete(limits = seq(from = 1870, to = 2014, by = 10)) +
-  ggtitle("Angola Annual Inflation From 1870 To 2014") 
-
-angola_inflation <- angola_inflation +
+  scale_x_discrete(limits = seq(from = min(angola$year), to = max(angola$year), by = 10)) +
+  ggtitle(paste(angola$country, "Annual Inflation From", min(angola$year), "To", max(angola$year))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
   geom_line(aes(x = year, y = 0))
 
+png("Inflation Rates/Angola.png")
 angola_inflation
+dev.off()
 
 # Central African Republican
 
@@ -71,21 +84,27 @@ car <- africa %>%
 
 car_exchange_rate <- ggplot(car, aes(x = year, y = exch_usd)) +
   geom_line() +   
-  scale_x_discrete(limits = seq(from = 1870, to = 2014, by = 10))
+  scale_y_discrete(limits = seq(from = min(car$exch_usd), to = max(car$exch_usd), by = 100)) +
+  ggtitle(paste(car$country, "Exchange Rates From", min(car$year), "To", max(car$year))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_line(aes(x = year, y = 0))
 
+png("Exchange Rates/Central African Republic.png")
 car_exchange_rate
+dev.off()
 
 # Inflation Rate
 
 car_inflation <- ggplot(car, aes(x = year, y = inflation_annual_cpi)) +
   geom_line() +
-  scale_x_discrete(limits = seq(from = 1870, to = 2014, by = 10)) +
-  ggtitle("Central African Republic Annual Inflation From 1870 To 2014") 
-
-car_inflation <- car_inflation +
+  scale_x_discrete(limits = seq(from = min(car$year), to = max(car$year), by = 10)) +
+  ggtitle(paste(car$country, "Annual Inflation From", min(car$year), "To", max(car$year))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
   geom_line(aes(x = year, y = 0))
 
+png("Inflation Rates/Central African Republic.png")
 car_inflation
+dev.off()
 
 # Ivory Coast
 
@@ -97,22 +116,95 @@ ivory_coast <- africa %>%
 
 # Exchange Rate
 
-ivory_coast__exchange_rate <- ggplot(ivory_coast, aes(x = year, y = exch_usd)) +
+ivory_coast_exchange_rate <- ggplot(ivory_coast, aes(x = year, y = exch_usd)) +
   geom_line() +   
-  scale_x_discrete(limits = seq(from = 1870, to = 2014, by = 10))
+  scale_y_discrete(limits = seq(from = min(ivory_coast$exch_usd), to = max(ivory_coast$exch_usd), by = 100)) +
+  ggtitle(paste(ivory_coast$country, "Exchange Rates From", min(ivory_coast$year), "To", max(ivory_coast$year))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_line(aes(x = year, y = 0))
 
+png("Exchange Rates/Ivory Coast.png")
 ivory_coast_exchange_rate
 
 # Inflation Rate
 
 ivory_coast_inflation <- ggplot(ivory_coast, aes(x = year, y = inflation_annual_cpi)) +
   geom_line() +
-  scale_x_discrete(limits = seq(from = 1870, to = 2014, by = 10)) +
-  ggtitle("Central African Republic Annual Inflation From 1870 To 2014") 
-
-ivory_coast_inflation <- ivory_coast_inflation +
+  scale_x_discrete(limits = seq(from = min(ivory_coast$year), to = max(ivory_coast$year), by = 10)) +
+  ggtitle(paste(ivory_coast$country, "Annual Inflation From", min(ivory_coast$year), "To", max(ivory_coast$year))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
   geom_line(aes(x = year, y = 0))
 
+png("Inflation Rates/Ivory Coast.png")
 ivory_coast_inflation
+dev.off()
+
+# Egypt
+
+rm(list=ls())
+africa <- read.csv("africa-economic-banking-and-systemic-crisis-data/african_crises.csv")
+
+egypt <- africa %>%
+  filter(country == "Egypt")
+
+# Exchange Rate
+
+egypt_exchange_rate <- ggplot(egypt, aes(x = year, y = exch_usd)) +
+  geom_line() +   
+  scale_y_discrete(limits = seq(from = min(egypt$exch_usd), to = max(egypt$exch_usd), by = 0.5)) +
+  ggtitle(paste(egypt$country, "Exchange Rates From", min(egypt$year), "To", max(egypt$year))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_line(aes(x = year, y = 0))
+
+png("Exchange Rates/Egypt.png")
+egypt_exchange_rate
+dev.off()
+
+# Inflation Rate
+
+egypt_inflation <- ggplot(egypt, aes(x = year, y = inflation_annual_cpi)) +
+  geom_line() +
+  scale_x_discrete(limits = seq(from = min(egypt$year), to = max(egypt$year), by = 10)) +
+  ggtitle(paste(egypt$country, "Annual Inflation From", min(egypt$year), "To", max(egypt$year))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_line(aes(x = year, y = 0))
+
+png("Inflation Rates/Egypt.png")
+egypt_inflation
+dev.off()
+
+# Kenya
+
+rm(list=ls())
+africa <- read.csv("africa-economic-banking-and-systemic-crisis-data/african_crises.csv")
+
+kenya <- africa %>%
+  filter(country == "Kenya")
+
+# Exchange Rate
+
+kenya_exchange_rate <- ggplot(kenya, aes(x = year, y = exch_usd)) +
+  geom_line() +
+  scale_y_discrete(limits = seq(from = min(kenya$exch_usd), to = max(kenya$exch_usd), by = 0.5)) +
+  ggtitle(paste(kenya$country, "Exchange Rates From", min(kenya$year), "To", max(kenya$year))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_line(aes(x = year, y = 0))
+
+png("Exchange Rates/Kenya.png")
+kenya_exchange_rate
+dev.off()
+
+# Inflation Rate
+
+kenya_inflation <- ggplot(kenya, aes(x = year, y = inflation_annual_cpi)) +
+  geom_line() +
+  scale_x_discrete(limits = seq(from = min(kenya$year), to = max(kenya$year), by = 10)) +
+  ggtitle(paste(kenya$country, "Annual Inflation From", min(kenya$year), "To", max(kenya$year))) +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  geom_line(aes(x = year, y = 0))
+
+png("Inflation Rates/Kenya.png")
+kenya_inflation
+dev.off()
 
 
